eb703ee028e82f0014e10487d73e0040941c980b
Kirigaya-Suguha <shiruitop@gmail.com>
2021-07-28 03:40:20 +0800

    birthday_1

--- config.js
+++ config.js
@@ -27,16 +27,16 @@ var config = {
      */
     imgs: {
         "这是你出生时发出光芒的那颗星星": "./imgs/star.png",
-        "这是7月27号的宇宙": "./imgs/stardust.jpeg",
+        //"这是7月27号的宇宙": "./imgs/stardust.jpeg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
+        play: "来点音乐",
+        bannar_coming: "来点氛围",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "怎么能没有蛋糕？",
+        light_candle: "点蜡烛！",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
